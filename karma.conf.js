616d8242b28b6bb9082f089c2722557860839ae9
Pawel <jarrodek@gmail.com>
2019-11-08 13:54:50 -0800

    test: adding JS depdnencies to the test build

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,26 @@ module.exports = (config) => {
             pattern: config.grep ? config.grep : 'test/**/*.test.js',
             type: 'module',
           },
+          {
+            pattern: require.resolve('prismjs/prism.js'),
+            type: 'js',
+          },
+          {
+            pattern: require.resolve('prismjs/components/prism-bash.min.js'),
+            type: 'js',
+          },
+          {
+            pattern: require.resolve('prismjs/components/prism-java.min.js'),
+            type: 'js',
+          },
+          {
+            pattern: require.resolve('prismjs/components/prism-python.min.js'),
+            type: 'js',
+          },
+          {
+            pattern: require.resolve('prismjs/components/prism-http.min.js'),
+            type: 'js',
+          },
         ],
 
         // see the karma-esm docs for all options
